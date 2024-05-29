e8cb179a695ae7bf163a83e3804019467948b1f6
matheushcandido <candidoma050@gmail.com>
2024-05-29 17:57:07 -0300

    preparando para cadastros de job completos

---
+++ Core/Entities/Enums/LevelEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Enums
+{
+    public enum LevelEnum
+    {
+        [Description("Júnior")]
+        Junior,
+
+        [Description("Pleno")]
+        MidLevel,
+
+        [Description("Sênior")]
+        Senior,
+
+        [Description("Especialista")]
+        Specialist
+    }
+}
--- Core/Entities/Job.cs
+++ Core/Entities/Job.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infrastructure.Data.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,19 @@ namespace Core.Entities
         public Guid Id { get; set; }
 
         public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string? Location { get; set; }
+
+        public string? Requirements { get; set; }
+
+        public string? Benefits { get; set; }
+
+        public WorkingModelEnum WorkingModel { get; set; }
+
+        public LevelEnum Level { get; set; }
     }
 }
